9b8f7cd2a506345da7d9758a427eb293e75c0988
Taian <tambrnp@163.com>
2015-11-07 04:24:46 +0800

    initial

---
+++ tMain.h
@@ -0,0 +1,13 @@
+//
+//  tMain.h
+//  example
+//
+//  Created by Hailan on 14-4-30.
+//  Copyright (c) 2014年 Hailan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tMain : NSObject
+
+@end
---
+++ tMain.m
@@ -0,0 +1,13 @@
+//
+//  tMain.m
+//  example
+//
+//  Created by Hailan on 14-4-30.
+//  Copyright (c) 2014年 Hailan. All rights reserved.
+//
+
+#import "tMain.h"
+
+@implementation tMain
+
+@end
